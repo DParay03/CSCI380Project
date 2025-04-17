e053ab9bbd9e446be5c11fbf77b9342e61ee7aa1
DParay03 <Dylan.Paray19@gmail.com>
2025-04-17 17:09:09 -0400

    profile update

--- communityPost/urls.py
+++ communityPost/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='communityPost-home'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('new/', PostCreateView.as_view(), name='post-create'),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
 ]
--- communityPost/views.py
+++ communityPost/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 # Create your views here.
@@ -14,3 +15,46 @@ def home(request):
 class PostListView(ListView):
     model = Post
     template_name = 'communityPost/communityPost.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'communityPost/communityPost_detail.html'
+    context_object_name = 'post'
+    ordering = ['date_posted']
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+    model = Post
+    template_name = 'communityPost/communityPost_detail.html'
+    context_object_name = 'post'
+    ordering = ['date_posted']
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
