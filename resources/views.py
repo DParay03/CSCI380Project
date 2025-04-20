0452a495aef6bb9de0dca3fd634a22310cb91d78
DParay03 <Dylan.Paray19@gmail.com>
2025-04-20 19:28:15 -0400

    mental health page styling, needs to include category part in template

--- resources/views.py
+++ resources/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Resource
 
 def resource_list(request):
-    resources = Resource.objects.all().order_by('category')
+    resources = Resource.objects.all().order_by('-created_at')
     return render(request, 'resources/resource_list.html', {'resources': resources})
 
 def resource_detail(request, pk):
